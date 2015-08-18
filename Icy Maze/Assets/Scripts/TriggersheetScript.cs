b5a1a5a96c0758f1617bfa3912e3a8ef9ba2e807
ks <ks945650@gmail.com>
2015-08-18 22:32:39 +0800

    pull things script

---
+++ Icy Maze/Assets/Scripts/TriggersheetScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggersheetScript : MonoBehaviour {
+	bool isOn;
+	// Use this for initialization
+	void Start () {
+		isOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision other){
+		if (other.collider.CompareTag ("ElementBox")) {
+			isOn = true;
+
+		}
+	
+}
+}
