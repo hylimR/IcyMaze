51e94f14c62f3be47313b1c5caed54ba69aa28e2
ks <ks945650@gmail.com>
2015-08-18 18:19:45 +0800

    trap box script

---
+++ Icy Maze/Assets/Scripts/TrapBoxScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapBoxScript : MonoBehaviour {
+	float x = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Translate (x*Time.deltaTime, 0f, 0f);	
+		
+	}
+	void OnTriggerEnter(){
+		x = x * -1;
+	}
+}
