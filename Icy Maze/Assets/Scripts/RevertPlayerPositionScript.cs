2c84bffc1f78f7644e91d0b568636fbe8ca38873
ks <ks945650@gmail.com>
2015-08-18 18:57:17 +0800

    player script in scene 6

---
+++ Icy Maze/Assets/Scripts/RevertPlayerPositionScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RevertPlayerPositionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(){
+		transform.position = new Vector3 (0f,0f,-13f);
+
+	}
+}
