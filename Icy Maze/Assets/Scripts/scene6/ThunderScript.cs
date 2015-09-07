ea6a267265ac5b90b20c283f660c2e31324fcc0d
ks <ks945650@gmail.com>
2015-09-07 21:53:49 +0800

    edit scene 6

--- Icy Maze/Assets/Scripts/scene6/ThunderScript.cs
+++ Icy Maze/Assets/Scripts/scene6/ThunderScript.cs
@@ -18,7 +18,7 @@ public class ThunderScript : MonoBehaviour {
 
 	void DisplayThunder(){
 		for (int i =0; i<targetPosition.Length; i++) {	//generate thunder around the trigger sheet
-			Instantiate (thunder,new Vector3(targetPosition[i].x,targetPosition[i].y+6f,targetPosition[i].z), Quaternion.identity);
+			Instantiate (thunder,new Vector3(targetPosition[i].x,targetPosition[i].y+5.7f,targetPosition[i].z), Quaternion.identity);
 		
 		}
 		Invoke ("DestroyThunder", 1f);
@@ -34,23 +34,23 @@ public class ThunderScript : MonoBehaviour {
 	}
 	void DisplayTarget(){
 		for (float i=0; i<2; i++) 
-		{	//generate thunder around the trigger sheet
-			Instantiate (target, new Vector3 (parent.x + Random.Range(-11f,-7.5f), parent.y , parent.z + i - 12f),Quaternion.identity);
+		{	//generate target around the trigger sheet
+			Instantiate (target, new Vector3 (parent.x + Random.Range(-11f,-7.5f), parent.y+0.3f , parent.z + i - 12f),Quaternion.identity);
 			
-			Instantiate (target, new Vector3 (parent.x  + Random.Range(11f,7.5f), parent.y , parent.z + i - 12f),Quaternion.identity);
+			Instantiate (target, new Vector3 (parent.x  + Random.Range(11f,7.5f), parent.y+0.3f , parent.z + i - 12f),Quaternion.identity);
 			
-			Instantiate (target, new Vector3 (parent.x + Random.Range(11f,-7.5f), parent.y , parent.z + i +8f),Quaternion.identity);
+			Instantiate (target, new Vector3 (parent.x + Random.Range(11f,-7.5f), parent.y+0.3f, parent.z + i +8f),Quaternion.identity);
 			
-			Instantiate (target, new Vector3 (parent.x + Random.Range(11f,-7.5f), parent.y , parent.z + i +8f),Quaternion.identity);
+			Instantiate (target, new Vector3 (parent.x + Random.Range(11f,-7.5f), parent.y+0.3f , parent.z + i +8f),Quaternion.identity);
 		}
 		for (int i =0 ;i<numberOfThunder-8;i++)
-		{//generate random thunder in the script;
-			Instantiate (target, new Vector3 (parent.x + Random.Range(7.5f,-7.5f), parent.y , parent.z + Random.Range(13f,-7f)),Quaternion.identity);
+		{//generate random target in the scene;
+			Instantiate (target, new Vector3 (parent.x + Random.Range(7.5f,-7.5f), parent.y+0.3f , parent.z + Random.Range(13f,-7f)),Quaternion.identity);
 		}
 		Invoke ("DestroyTarget", 1f);
 	}
 	void DestroyTarget()
-	{ // destory the thunder
+	{ // destory the target
 		targetTobeDestroy = GameObject.FindGameObjectsWithTag ("target");
 		for (int i=0; i<targetTobeDestroy.Length; i++) {
 			targetPosition[i]=targetTobeDestroy[i].transform.position;
